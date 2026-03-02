3138149dbf5a63879122b7cf3a33627839164545
Khen chandarapisey <piseykhenchandara93@gmail.com>
2026-03-02 09:11:02 +0700

    modify : restauant_comments_view.dart, restaurant_type.chip.dart

--- lib/ui/screens/restaurant_comments_view.dart
+++ lib/ui/screens/restaurant_comments_view.dart
@@ -1,1 +1,52 @@
-// YOUR CODE
+import 'package:final_exam/models/restaurant.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantCommentsView extends StatelessWidget {
+  const RestaurantCommentsView({super.key, required this.restaurant});
+
+  final Restaurant restaurant;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.blue,
+            padding: EdgeInsets.all(10),
+            child: Column(
+
+              children: [
+                Text(restaurant.name, style: TextStyle(fontSize: 20)),
+                Text(restaurant.address),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.blue, width: 2),
+                      ),
+                      child: Text(restaurant.type.name),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: Colors.blue, width: 2),
+                      ),
+                      child: Chip(
+                        avatar: Icon(Icons.star, size: 18),
+                        label: Text("4.7", textAlign: TextAlign.center),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/widgets/chip/restaurant_type_chip.dart
+++ lib/ui/widgets/chip/restaurant_type_chip.dart
@@ -1,17 +1,18 @@
 import 'package:final_exam/models/restaurant.dart';
+import 'package:final_exam/ui/screens/restaurant_comments_view.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantTypeChip extends StatelessWidget {
   const RestaurantTypeChip({super.key, required this.restaurant});
 
   final Restaurant restaurant;
-  
+
   @override
   Widget build(BuildContext context) {
     return Column(
-     
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           margin: EdgeInsets.all(15),
@@ -19,43 +20,49 @@ class RestaurantTypeChip extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(restaurant.name, textAlign: TextAlign.start,),
-              Row(children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    border: Border.all(
+              Text(restaurant.name, textAlign: TextAlign.start),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
                         color: Colors.red, // border color
                         width: 2, // border thickness
                       ),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.star),
+                        SizedBox(width: 15),
+                        Text("0.0"),
+                      ],
+                    ),
                   ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.star),
-                      SizedBox(width: 15,),
-                      Text("0.0")
-                    ],
+                  SizedBox(width: 15),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.red, width: 2),
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                RestaurantCommentsView(restaurant: restaurant),
+                          ),
+                        );
+                      },
+                      child: Text(restaurant.type.name),
+                    ),
                   ),
-                ),
-                SizedBox(width: 15,),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.red,
-                      width: 2,
-                    
-                    )
-                  ),
-                  child: Text(restaurant.type.name ),
-                )
-              ],)
+                ],
+              ),
             ],
           ),
-              
-              
-          
-        )
+        ),
       ],
     );
   }
